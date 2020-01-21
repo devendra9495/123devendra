d43e13fccc414ab7b105ed99c8a78a7a1748f67e
devendra9495 <59868659+devendra9495@users.noreply.github.com>
2020-01-21 16:14:36 +0530

    Add files via upload

---
+++ reverse_array.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl -w
+#Reverse of an array
+@bases =('A','C','G','T');
+@reverse=reverse @bases;
+print"Here's the array in reverse:";
+print"@reverse\n\n";
+print"=====================";
+print"\n\n";
+exit;
+

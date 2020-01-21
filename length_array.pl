6f7c52f176673330b1cd823e7f59dafdd4d76964
devendra9495 <59868659+devendra9495@users.noreply.github.com>
2020-01-21 16:20:50 +0530

    Add files via upload

---
+++ length_array.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+# length of an arrays
+@bases =('A','C','G','T');
+print"Here's the length of the array:";
+print scalar @bases, "\n";
+print"=====================";
+print"\n\n";
+exit;
+

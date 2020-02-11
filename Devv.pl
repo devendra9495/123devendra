98aea18b11bdc133fa5c424ceb53b1b1a6b46422
devendra9495 <59868659+devendra9495@users.noreply.github.com>
2020-02-11 17:01:02 +0530

    Add files via upload

---
+++ Devv.pl
@@ -0,0 +1,16 @@
+# !user/bin/perl-w
+# A program with a subroutine to append ACGT to DNA
+# The original DNA
+$dna='CTGGAATCGATTGACA';
+#The call to the subroutine "addACGT".
+#The argument being passed in is $dna; the result is saved in $longer dna
+$longer_dna= addACGT($dna);
+print"I added ACGT to $dna and got $longer_dna\n\n";
+exit;
+#Here is the definition for subroutine "addACGT"
+sub addACGT
+{ 
+my($dna)=@_;
+$dna.='ACGT';
+return $dna;
+}
